c1c952cd5cfd6a714bfe76be6258adb24475dbd6
Kanakaraju Gandreddi <Kanakaraju.Gandreddi@ril.com>
2020-06-20 20:44:32 +0530

    changs

---
+++ DemoSDK-Kanak/DemoSDK-Kanak/DemoSDKServiceManager.swift
@@ -0,0 +1,16 @@
+//
+//  DemoSDKServiceManager.swift
+//  DemoSDK
+//
+//  Created by Kanakaraju Gandreddi on 20/06/20.
+//  Copyright © 2020 Kanakaraju Gandreddi. All rights reserved.
+//
+
+import Foundation
+public class DemoSDKServiceManager {
+    private init(){}
+    public static func doSomething() -> String {
+        return "This is from Demo SDK...."
+    }
+    
+}
